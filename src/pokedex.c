938b2b5770fbf6ab4d96a9edfa1aa986620210ac
Flash1Lucky <ericflashrussell@gmail.com>
2026-02-25 00:30:59 -0500

    Added Debug Pokedex Flag Toggles
    Added options to the debug menu for toggling having the pokedex, national dex, setting all of the entry flags, or resetting all of the entry flags to only pokemon on the team or in the box.

--- src/pokedex.c
+++ src/pokedex.c
@@ -1067,6 +1067,12 @@ void Pokedex_ObtainNationalDex(Pokedex *pokedexData)
     pokedexData->nationalDexObtained = TRUE;
 }
 
+void Pokedex_DisableNationalDex(Pokedex *pokedexData)
+{
+    CheckPokedexIntegrity(pokedexData);
+    pokedexData->nationalDexObtained = FALSE;
+}
+
 BOOL Pokedex_IsNationalDexObtained(const Pokedex *pokedex)
 {
     CheckPokedexIntegrity(pokedex);
@@ -1119,6 +1125,22 @@ void Pokedex_ObtainPokedex(Pokedex *pokedex)
 {
     CheckPokedexIntegrity(pokedex);
     pokedex->pokedexObtained = TRUE;
+}
+
+void Pokedex_DisablePokedex(Pokedex *pokedex)
+{
+    CheckPokedexIntegrity(pokedex);
+    pokedex->pokedexObtained = FALSE;
+}
+
+void Pokedex_SetAllSeenCaught(Pokedex *pokedex)
+{
+    CheckPokedexIntegrity(pokedex);
+
+    for (u16 species = 1; species <= NATIONAL_DEX_COUNT; species++) {
+        Write_SeenSpecies(pokedex, species);
+        Write_CaughtSpecies(pokedex, species);
+    }
 }
 
 Pokedex *SaveData_GetPokedex(SaveData *saveData)
